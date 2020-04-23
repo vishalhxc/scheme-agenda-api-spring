26d45ec403e30a1d704a68ffd86837a2e0a3b80b
vishalhxc <vishalhxc@gmail.com>
2020-04-23 09:42:04 -0500

    initial scheme commit

---
+++ src/main/kotlin/com/scheme/routinetrackerapi/RoutineTrackerApiApplication.kt
@@ -0,0 +1,11 @@
+package com.scheme.routinetrackerapi
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class RoutineTrackerApiApplication
+
+fun main(args: Array<String>) {
+	runApplication<RoutineTrackerApiApplication>(*args)
+}
---
+++ src/test/kotlin/com/scheme/routinetrackerapi/RoutineTrackerApiApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.scheme.routinetrackerapi
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class RoutineTrackerApiApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
